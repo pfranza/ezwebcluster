b1bf4df20b8457e42cc4cd14978666d9c5cce3fb
pfranza@gmail.com <pfranza@gmail.com@28e9aae1-cc3e-6947-e67c-847ed59a7836>
2012-04-10 00:01:37 +0000

    adding protocol buffer support

--- modules/common/src/com/gorthaur/cluster/channels/AdministrationChannel.java
+++ modules/common/src/com/gorthaur/cluster/channels/AdministrationChannel.java
@@ -1,5 +1,54 @@
 package com.gorthaur.cluster.channels;
 
+import java.util.Collection;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.jgroups.Address;
+import org.jgroups.JChannel;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
+
+@Singleton
 public class AdministrationChannel {
 
+	private JChannel channel;
+
+	private ReceiverAdapter recv = new ReceiverAdapter() {
+		
+		public void receive(org.jgroups.Message msg) {
+			
+		}
+		
+		public void viewAccepted(View newView) {
+			 System.out.println("** view: " + newView);
+		}
+		
+	};
+	
+	@Inject
+	AdministrationChannel() throws Exception {
+		channel=new JChannel();
+		channel.setReceiver(recv);
+		channel.connect("AdministrationChannel");
+		System.out.println("Local Name: " + channel.getName() );	
+	}
+	
+	public Collection<Address> getAddresses() {
+		return channel.getView().getMembers();
+	}
+	
+//	private static class ClusterNode implements Serializable {
+//
+//		/**
+//		 * 
+//		 */
+//		private static final long serialVersionUID = 3849098560672418456L;
+//		
+//		private Address address;
+//		
+//	}
+//	
+	
 }
