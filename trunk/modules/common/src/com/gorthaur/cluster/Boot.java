8699aa715f16b5a7baa30b32fe84dbf01f5fa9db
pfranza@gmail.com <pfranza@gmail.com@28e9aae1-cc3e-6947-e67c-847ed59a7836>
2012-04-10 01:04:24 +0000

    adding some boiler plate console messaging

--- trunk/modules/common/src/com/gorthaur/cluster/Boot.java
+++ trunk/modules/common/src/com/gorthaur/cluster/Boot.java
@@ -2,6 +2,14 @@ package com.gorthaur.cluster;
 
 import javax.inject.Inject;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 
 public class Boot {
@@ -9,12 +17,45 @@ public class Boot {
 	@Inject
 	ClusterStateManager channelManager;
 	
+	@Inject CommandLine commandLine;
+	
 	void start() {
-		
+		if(commandLine.hasOption("console")) {
+			String consolePort = commandLine.getOptionValue("console");
+			System.out.println("Console On Port " + consolePort);
+		} else {
+			System.out.println("Launch Slave");
+		}
 	}
 	
-	public static void main(String[] args) {
-		Guice.createInjector().getInstance(Boot.class).start();		
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) throws Exception {
+		
+		Options options = new Options();
+		options.addOption(OptionBuilder.withArgName("port")
+				.hasArg()
+				.withDescription("start a management console")
+				.create("console"));
+		
+		CommandLineParser parser = new GnuParser();
+		try {
+			final CommandLine line = parser.parse( options, args );
+			
+		    
+			Guice.createInjector(new AbstractModule(){
+
+				@Override
+				protected void configure() {
+					bind(CommandLine.class).toInstance(line);
+				}
+				
+			}).getInstance(Boot.class).start();	
+			
+		} catch (Exception ex) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp( "<jarname>", options );
+		}
+	
 	}
 	
 }
---
+++ trunk/modules/console/src/com/gorthaur/cluster/console/client/AppPlaceHistoryMapper.java
@@ -0,0 +1,24 @@
+package com.gorthaur.cluster.console.client;
+
+import com.google.gwt.place.shared.PlaceHistoryMapper;
+import com.google.gwt.place.shared.WithTokenizers;
+
+@WithTokenizers({
+//	AdministrationPlace.Tokenizer.class,
+//	SkillsPlace.Tokenizer.class,
+//	CharactersPlace.Tokenizer.class,
+//	DialogPlace.Tokenizer.class,
+//	GlossaryPlace.Tokenizer.class,
+//	ChallengePlace.Tokenizer.class,
+//	TOCManagerPlace.Tokenizer.class,
+//	TOCUnitManagerPlace.Tokenizer.class,
+//	LessonPlace.Tokenizer.class,
+//	ProblemsPlace.Tokenizer.class,
+//	ProblemGroupPlace.Tokenizer.class,
+//	LessonVideoEditorPlace.Tokenizer.class,
+//	LessonTextEditorPlace.Tokenizer.class,
+//	PracticeProblemEditorPlace.Tokenizer.class,
+//	QAProblemPlace.Tokenizer.class,
+//	QAProblemSummaryPlace.Tokenizer.class
+})
+public interface AppPlaceHistoryMapper extends PlaceHistoryMapper {}
